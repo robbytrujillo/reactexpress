e7762da0febe91ca3a6af83bc74c28c64162479e
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-07-22 07:25:52 +0700

    template String atau literal

---
+++ templateStringAtauLiteral.js
@@ -0,0 +1,17 @@
+// Contoh menggabungkan string dan variable tanpa template string
+
+let name = "Robby Ilhamkusuma";
+
+let gender = "Laki-laki";
+
+console.log(name + "memiliki jenis kelamin : " + gender);
+// output "Robby Ilhamkusuma memiliki jensis kelamin : Laki-laki"
+
+// jika menuliskannya menggunakan template string
+
+let name1 = "Robby Ilhamkusuma";
+
+let gender1 = "Laki-laki";
+
+console.log(`${name1} memiliki jenis kelamin : ${gender1}`);
+// output "Robby Ilhamkusuma memiliki jensis kelamin : Laki-laki"
